b6b3d6dafb75124a9fad39f5f26e1ddf4f467c94
Igor Kakupshev <ivkakupshev@yandex.ru>
2021-02-17 12:28:34 +0500

    feat: update userCreate controller. step 1.02 of 20

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,19 +4,18 @@ const validator = require('validator');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
-    required: true,
     validate: {
       validator(v) {
         return /^https?:\/\/(www\.)?[0-9a-z\-._~:/?#[\]@!$&'()*+,;=]{1,}/i.test(
@@ -25,6 +24,8 @@ const userSchema = new mongoose.Schema({
       },
       message: 'Не корректный URL',
     },
+    default:
+      'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
     type: String,
@@ -40,6 +41,7 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
+    minlength: 3,
   },
 });
 
